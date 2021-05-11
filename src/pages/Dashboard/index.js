16db3aa334c0fa282f653df305b2949fe4dc9479
Novita <novitaguok@gmail.com>
2021-05-12 02:20:49 +0700

    Get user from local storage at dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default function Dashboard() {
+    const user_id = localStorage.getItem('user')
+    console.log(user_id)
+
     return (
         < div >
             Hello from Dashboard
