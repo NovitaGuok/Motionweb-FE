c6107fbf702caec6f880c49d045f2c4ef680bdaf
Novita <novitaguok@gmail.com>
2021-05-11 10:49:17 +0700

    Restructuring frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+import Routes from './routes'
+import './App.css';
+
+function App() {
+  return (
+    <Container>
+      <h1>Mobile Innovation</h1>
+      <Routes></Routes>
+    </Container>
+  );
+}
+
+export default App;
