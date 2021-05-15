90bb733c68ba343df8bc5941981ae9dab5bcbe5e
Novita <novitaguok@gmail.com>
2021-05-15 14:42:31 +0700

    Create event page UI

--- src/pages/EventsPage/index.js
+++ src/pages/EventsPage/index.js
@@ -4,14 +4,15 @@ import { Container, Button, Form, FormGroup, Label, Input, Alert } from 'reactst
 import cameraIcon from '../../assets/camera.png'
 import './events.css'
 
-export default function EventsPage() {
+export default function EventsPage({ history }) {
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
     const [price, setPrice] = useState('')
     const [thumbnail, setThumbnail] = useState(null)
     const [category, setCategory] = useState('')
     const [date, setDate] = useState('')
-    const [errorMessage, setErrorMessage] = useState(false)
+    const [error, setError] = useState(false)
+    const [success, setSuccess] = useState(false)
 
     const preview = useMemo(() => {
         return thumbnail ? URL.createObjectURL(thumbnail) : null
@@ -33,15 +34,15 @@ export default function EventsPage() {
         try {
             if (title !== "" && description !== "" && price !== "" && category !== "" && date !== "" && thumbnail !== null) {
                 await api.post('/event', eventData, { headers: { user_id } })
-                console.log(user_id, title, description, price, category, date)
-
-            } else {
-                setErrorMessage(true)
+                setSuccess(true)
                 setTimeout(() => {
-                    setErrorMessage(false)
+                    setSuccess(false)
                 }, 2000);
-
-                console.log("Missing required data!")
+            } else {
+                setError(true)
+                setTimeout(() => {
+                    setError(false)
+                }, 2000);
             }
         } catch (error) {
             Promise.reject(error)
@@ -53,41 +54,53 @@ export default function EventsPage() {
         <Container>
             <h1>Create your Event</h1>
             <Form onSubmit={submitHandler}>
+                <div className="input-group">x
                 <FormGroup>
-                    <FormGroup>
-                        <Label>Upload Image: </Label>
-                        <Label id="thumbnail" style={{ backgroundImage: `url(${preview})` }} className={thumbnail ? 'has-thumbnail' : ''} >
-                            <Input type="file" onChange={evt => setThumbnail(evt.target.files[0])} />
-                            <img src={cameraIcon} style={{ maxWidth: "50px" }} aria-hidden alt="Upload icon image" />
-                        </Label>
+                        <FormGroup>
+                            <Label>Upload Image: </Label>
+                            <Label id="thumbnail" style={{ backgroundImage: `url(${preview})` }} className={thumbnail ? 'has-thumbnail' : ''} >
+                                <Input type="file" onChange={evt => setThumbnail(evt.target.files[0])} />
+                                <img src={cameraIcon} style={{ maxWidth: "50px" }} aria-hidden alt="Upload icon image" />
+                            </Label>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>Category: </Label>
+                            <Input id="category" type="text" value={category} placeholder={'Event category'} onChange={(evt) => setCategory(evt.target.value)} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>Title: </Label>
+                            <Input id="title" type="text" value={title} placeholder={'Event title'} onChange={(evt) => setTitle(evt.target.value)} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>Description: </Label>
+                            <Input id="description" type="text" value={description} placeholder={'Event description'} onChange={(evt) => setDescription(evt.target.value)} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>Price: </Label>
+                            <Input id="price" type="number" value={price} placeholder={'Event price Rp0.0'} onChange={(evt) => setPrice(evt.target.value)} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>Date: </Label>
+                            <Input id="date" type="date" value={date} placeholder={'Event date'} onChange={(evt) => setDate(evt.target.value)} />
+                        </FormGroup>
                     </FormGroup>
-                    <FormGroup>
-                        <Label>Category: </Label>
-                        <Input id="category" type="text" value={category} placeholder={'Event category'} onChange={(evt) => setCategory(evt.target.value)} />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>Title: </Label>
-                        <Input id="title" type="text" value={title} placeholder={'Event title'} onChange={(evt) => setTitle(evt.target.value)} />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>Description: </Label>
-                        <Input id="description" type="text" value={description} placeholder={'Event description'} onChange={(evt) => setDescription(evt.target.value)} />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>Price: </Label>
-                        <Input id="price" type="number" value={price} placeholder={'Event price Rp0.0'} onChange={(evt) => setPrice(evt.target.value)} />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>Date: </Label>
-                        <Input id="date" type="date" value={date} placeholder={'Event date'} onChange={(evt) => setDate(evt.target.value)} />
-                    </FormGroup>
-                    <Button type="submit">
+                </div>
+                <FormGroup>
+                    <Button className="submit-btn">
                         Create Event
-                    </Button>
+                        </Button>
+                </FormGroup>
+                <FormGroup>
+                    <Button className="secondary-btn" onClick={() => history.push('/dashboard')}>
+                        Dashboard
+                        </Button>
                 </FormGroup>
             </Form>
-            {errorMessage ? (
+            {error ? (
                 <Alert className="event-validation" color="danger">Missing required information</Alert>
+            ) : ""}
+            {success ? (
+                <Alert className="event-validation" color="success">Event created successfully!</Alert>
             ) : ""}
         </Container>
     )
